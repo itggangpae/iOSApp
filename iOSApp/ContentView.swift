f85577f7d4e1a3a12b2799155258540319195005
adam <ggangpae1@gmail.com>
2020-08-13 06:30:31 +0900

    Initial Commit

---
+++ iOSApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSApp
+//
+//  Created by Munseok Park on 2020/08/13.
+//  Copyright © 2020 Munseok Park. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
